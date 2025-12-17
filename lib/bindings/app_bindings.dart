7bfabb55770aadc5a15f33059321bf610693b2a9
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-17 23:38:51 +0500

    added login controller

--- lib/bindings/app_bindings.dart
+++ lib/bindings/app_bindings.dart
@@ -1,6 +1,11 @@
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/bindings_interface.dart';
+import 'package:my_events/features/authentication/controller/login/login_controller.dart';
 
 class AppBindings with Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put(LoginController(), permanent: true);
+  }
 }
