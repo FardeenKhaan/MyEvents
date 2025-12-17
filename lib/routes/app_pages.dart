9c2b5700743b918b715c94cac49c138335715de1
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-17 23:39:09 +0500

    paths added for now

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:my_events/features/authentication/screens/home/home_screen.dart';
 import 'package:my_events/features/authentication/screens/logIn/login_screen.dart';
 import 'package:my_events/features/splash/splash_screen.dart';
 import 'package:my_events/routes/app_routes.dart';
@@ -7,8 +8,6 @@ class AppPages {
   static final pages = [
     GetPage(name: AppRoutes.splashScreen, page: () => const SplashScreen()),
     GetPage(name: AppRoutes.loginScreen, page: () => const LoginScreen()),
-    // GetPage(name: AppRoutes.navigationMenu, page: () => const NavigationMenu(), binding: NavigationBinding()),
-    // GetPage(name: AppRoutes.signupScreen, page: () => SignupScreen()),
-    // GetPage(name: AppRoutes.loginScreen, page: () => LoginScreen()),
+    GetPage(name: AppRoutes.homeScreen, page: () => HomeScreen()),
   ];
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,12 +2,7 @@
 
 abstract class AppRoutes {
   static const splashScreen = '/';
-  static const onboarding = '/onboarding';
-  static const navigationMenu = '/navigationMenu';
-  // static const subscriptionPerks = '/subscription_perks';
   static const signupScreen = '/signup';
   static const loginScreen = '/login';
-  static const privacyPolicyScreen = '/privacy_policy';
-  static const termsConditionsScreen = '/terms_conditions';
   static const homeScreen = '/home';
 }
