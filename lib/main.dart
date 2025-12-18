97387d7518b642faff7d717c4d84076b37bbd6a2
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-18 23:44:11 +0500

    setup for getIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_events/app.dart';
+import 'package:my_events/data/services/get_it/get_it.dart';
 
 void main() async {
   /// Widgets Flutter Binding
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// Register Get It Singletons
+  GetItServices.setupSingletons();
 
   /// Run App
   runApp(const MyApp());
