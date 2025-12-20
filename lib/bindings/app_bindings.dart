32075fd2fbd78a6164c74ef3f18383ed80fc666b
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-20 23:53:16 +0500

    added connectivity controller

--- lib/bindings/app_bindings.dart
+++ lib/bindings/app_bindings.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:my_events/features/authentication/controller/login/login_controller.dart';
+import 'package:my_events/features/home/controller/connectivity_controller.dart';
 
 class AppBindings with Bindings {
   @override
   void dependencies() {
     Get.put(LoginController(), permanent: true);
+    Get.put(ConnectivityController(), permanent: true);
   }
 }
