fa37bb4cd537731cf47746e57bf8db63c8086218
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-20 23:54:32 +0500

    created method used for token storage

--- lib/utils/storage/secure_storage.dart
+++ lib/utils/storage/secure_storage.dart
@@ -1,17 +1,22 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:my_events/utils/constants/api_keys.dart';
 
 class SecureStorage {
+  // Create storage
   static const _storage = FlutterSecureStorage();
-  static const _tokenKey = 'auth_token';
+  static const _tokenKey = FkApiKeys.secureStorageToken;
 
+  // Save token
   static Future<void> saveToken(String token) async {
     await _storage.write(key: _tokenKey, value: token);
   }
 
+  // Get token
   static Future<String?> getToken() async {
     return await _storage.read(key: _tokenKey);
   }
 
+  // Clear token
   static Future<void> clearToken() async {
     await _storage.delete(key: _tokenKey);
   }
