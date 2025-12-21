ab3d26a28e657c9fed63907fb44ebffdffdc5922
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-21 21:44:24 +0500

    created authentication repository using reqres.in

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,19 +1,59 @@
 import 'package:dio/dio.dart';
-import 'package:my_events/data/services/api/api_service.dart';
+import 'package:my_events/data/models/api_response_model.dart';
+import 'package:my_events/data/services/api/auth_api_service.dart';
+import 'package:my_events/data/services/get_it/get_it.dart';
+import 'package:my_events/env/api_keys.dart';
+import 'package:my_events/env/api_urls.dart';
 import '../models/auth_response.dart';
 
 class AuthRepository {
-  final Dio _dio = ApiService.dio;
+  /// use GetIt
+  final ApiServices _apiServices = getIt<ApiServices>();
 
+  // -- ReqRes for login APi
   Future<AuthResponse> login({required String email, required String password}) async {
-    final response = await _dio.post('/login', data: {'email': email, 'password': password});
+    try {
+      /// API endpoint
+      String url = ApiUrls.login;
+      // ready the data
+      final body = {FkApiKeys.email: email, FkApiKeys.password: password};
 
-    return AuthResponse.fromJson(response.data);
+      /// Request body
+      ApiResponseModel response = await _apiServices.postApi(url: url, body: body);
+
+      /// check response
+      if (response.success == true) {
+        return AuthResponse.fromJson(response.data);
+      }
+
+      throw response.message;
+    } on DioException catch (e) {
+      throw e.message ?? 'Login failed';
+    }
   }
 
+  // -- ReqRes for SignUp APi
   Future<AuthResponse> register({required String email, required String password}) async {
-    final response = await _dio.post('/register', data: {'email': email, 'password': password});
+    try {
+      /// API endpoint
+      String url = ApiUrls.register;
+      // ready the data
+      final body = {FkApiKeys.email: email, FkApiKeys.password: password};
 
-    return AuthResponse.fromJson(response.data);
+      /// Request body
+      ApiResponseModel response = await _apiServices.postApi(
+        url: url,
+        body: body,
+        // {'email': email, 'password': password}
+      );
+
+      /// check response
+      if (response.success == true) {
+        return AuthResponse.fromJson(response.data);
+      }
+      throw response.message;
+    } on DioException catch (e) {
+      throw e.message ?? 'Registration failed';
+    }
   }
 }
