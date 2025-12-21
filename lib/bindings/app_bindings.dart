e28e42155e95364ef151f7d4cb0795d48c779c74
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-21 21:31:56 +0500

    updated binding structure

--- lib/bindings/app_bindings.dart
+++ lib/bindings/app_bindings.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 import 'package:my_events/features/authentication/controller/login/login_controller.dart';
 import 'package:my_events/features/home/controller/connectivity_controller.dart';
+import 'package:my_events/features/home/controller/event_controller.dart';
 
 class AppBindings with Bindings {
   @override
   void dependencies() {
-    Get.put(LoginController(), permanent: true);
     Get.put(ConnectivityController(), permanent: true);
+    Get.put(LoginController(), permanent: true);
+    Get.put(EventController(), permanent: true);
   }
 }
