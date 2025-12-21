c9cfdc1f793530e16a907bc6cf4190fc342805ba
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-21 21:51:26 +0500

    created for local storage

--- lib/data/services/local_storage/local_storage_service.dart
+++ lib/data/services/local_storage/local_storage_service.dart
@@ -1,59 +1,34 @@
 import 'package:get_storage/get_storage.dart';
-
-// class LocalStorageService {
-//   static final _box = GetStorage();
-
-//   // 🔑 Keys
-//   static const _eventsKey = 'cached_events';
-//   static const _eventDetailPrefix = 'cached_event_';
-
-//   /// Save event list
-//   static void saveEvents(List<Map<String, dynamic>> events) {
-//     _box.write(_eventsKey, events);
-//   }
-
-//   /// Read event list
-//   static List<Map<String, dynamic>>? getEvents() {
-//     return _box.read<List>(_eventsKey)?.cast<Map<String, dynamic>>();
-//   }
-
-//   /// Save single event detail
-//   static void saveEventDetail(String id, Map<String, dynamic> event) {
-//     _box.write('$_eventDetailPrefix$id', event);
-//   }
-
-//   /// Read single event detail
-//   static Map<String, dynamic>? getEventDetail(String id) {
-//     return _box.read<Map<String, dynamic>>('$_eventDetailPrefix$id');
-//   }
-
-//   static void clearAll() {
-//     _box.erase();
-//   }
-// }
+import 'package:my_events/env/keys.dart';
 
 class LocalStorageService {
+  // Local storage box
   static final _box = GetStorage();
 
-  static const _eventsKey = 'cached_events';
-  static const _eventDetailPrefix = 'cached_event_';
+  static const _eventsKey = FkKeys.eventsKey;
+  static const _eventDetailPrefix = FkKeys.eventDetailPrefix;
 
+  // Save list of events
   static void saveEvents(List<Map<String, dynamic>> events) {
     _box.write(_eventsKey, events);
   }
 
+  // Get list of cached events
   static List<Map<String, dynamic>>? getEvents() {
     return _box.read<List>(_eventsKey)?.cast<Map<String, dynamic>>();
   }
 
+  // Save event detail
   static void saveEventDetail(String id, Map<String, dynamic> event) {
     _box.write('$_eventDetailPrefix$id', event);
   }
 
+  // Clear cached events
   static void clearEvents() {
-    _box.remove('cached_events');
+    _box.remove(FkKeys.eventsKey);
   }
 
+  // Get cached event detail
   static Map<String, dynamic>? getEventDetail(String id) {
     return _box.read<Map<String, dynamic>>('$_eventDetailPrefix$id');
   }
