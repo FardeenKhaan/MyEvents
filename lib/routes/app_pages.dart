11aabddf39d045450d2a7e963900a0ac0b073280
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-21 21:31:12 +0500

    added routing paths

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,4 +1,7 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:my_events/features/authentication/screens/permissions/privacy_policy_screen.dart';
+import 'package:my_events/features/authentication/screens/permissions/terms_conditions_screen.dart';
+import 'package:my_events/features/authentication/screens/signUp/signup_screen.dart';
 import 'package:my_events/features/home/create_event/create_event_screen.dart';
 import 'package:my_events/features/home/event_detail/event_detail_screen.dart';
 import 'package:my_events/features/home/home_screen/home_screen.dart';
@@ -10,6 +13,9 @@ class AppPages {
   static final pages = [
     GetPage(name: AppRoutes.splashScreen, page: () => const SplashScreen()),
     GetPage(name: AppRoutes.loginScreen, page: () => const LoginScreen()),
+    GetPage(name: AppRoutes.signupScreen, page: () => const SignUpScreen()),
+    GetPage(name: AppRoutes.termsConditionsScreen, page: () => const TermsConditionsScreen()),
+    GetPage(name: AppRoutes.privacyPolicyScreen, page: () => const PrivacyPolicyScreen()),
     GetPage(name: AppRoutes.homeScreen, page: () => HomeScreen()),
     GetPage(name: AppRoutes.eventDetailScreen, page: () => EventDetailScreen()),
     GetPage(name: AppRoutes.createEvent, page: () => const CreateEditEventScreen()),
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,6 +4,8 @@ abstract class AppRoutes {
   static const splashScreen = '/';
   static const signupScreen = '/signup';
   static const loginScreen = '/login';
+  static const termsConditionsScreen = '/terms-conditions';
+  static const privacyPolicyScreen = '/privacy-policy';
   static const homeScreen = '/home';
   static const eventDetailScreen = '/event-detail';
   static const createEvent = '/create-event';
